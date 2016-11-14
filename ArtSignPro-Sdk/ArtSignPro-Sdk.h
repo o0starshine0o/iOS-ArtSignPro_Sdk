862be30e9f8af5186e574d86b1adbf9a072f286e
AbelHu <tohys@qq.com>
2016-11-14 12:33:35 +0800

    init

---
+++ ArtSignPro-Sdk/ArtSignPro-Sdk.h
@@ -0,0 +1,19 @@
+//
+//  ArtSignPro-Sdk.h
+//  ArtSignPro-Sdk
+//
+//  Created by AbelHu on 2016/11/14.
+//  Copyright © 2016年 qicode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArtSignPro-Sdk.
+FOUNDATION_EXPORT double ArtSignPro_SdkVersionNumber;
+
+//! Project version string for ArtSignPro-Sdk.
+FOUNDATION_EXPORT const unsigned char ArtSignPro_SdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArtSignPro_Sdk/PublicHeader.h>
+
+
